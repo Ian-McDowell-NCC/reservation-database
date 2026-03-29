f9597db598217d1e662a97d717329f96a9bc47f7
Raymond Castro <raymond.castro@student.northampton.edu>
2026-03-29 00:32:41 +0000

    Add uppercase to SQL keywords

--- ReservationDatabase.sql
+++ ReservationDatabase.sql
@@ -14,133 +14,133 @@ DROP TABLE IF EXISTS Restaurant;
 
 
 CREATE TABLE SystemUser (
- UserID int,
- UserFirstName varchar(15),
- UserLastName varchar(15),
- UserPhone varchar(20),
- UserEmail varchar(50),
- UserRole char(3),
+ UserID INT,
+ UserFirstName VARCHAR(15),
+ UserLastName VARCHAR(15),
+ UserPhone VARCHAR(20),
+ UserEmail VARCHAR(50),
+ UserRole CHAR(3),
  PRIMARY KEY (UserID)
  );
 
  CREATE TABLE Restaurant(
- RestaurantID int,
- RestaurantName varchar(50) NOT NULL,
- RestaurantAddress varchar(50) NOT NULL,
- SeatingCapacity int NOT NULL,
- FloorPlan varbinary(max),
- Menu varbinary(max),
+ RestaurantID INT,
+ RestaurantName VARCHAR(50) NOT NULL,
+ RestaurantAddress VARCHAR(50) NOT NULL,
+ SeatingCapacity INT NOT NULL,
+ FloorPlan VARBINARY(max),
+ Menu VARBINARY(max),
  PRIMARY KEY (RestaurantID)
  );
 
  CREATE TABLE Reservation (
- ReservationID int,
- ResDate date NOT NULL, 
- ResTime time NOT NULL,
- PartySize int NOT NULL,
- PartyName varchar(30) NOT NULL,
- ResPhone varchar(20),
- ResEmail varchar(30),
- OptIns bit NOT NULL,
- ResStatus char(10) DEFAULT 'IN FUTURE' NOT NULL,
- DateCreated smalldatetime NOT NULL,
- Comments varchar(max),
- RestaurantAt int,
- AssignedToTable int DEFAULT NULL,
+ ReservationID INT,
+ ResDate DATE NOT NULL, 
+ ResTime TIME NOT NULL,
+ PartySize INT NOT NULL,
+ PartyName VARCHAR(30) NOT NULL,
+ ResPhone VARCHAR(20),
+ ResEmail VARCHAR(30),
+ OptIns BIT NOT NULL,
+ ResStatus CHAR(10) DEFAULT 'IN FUTURE' NOT NULL,
+ DateCreated SMALLDATETIME NOT NULL,
+ Comments VARCHAR(max),
+ RestaurantAt INT,
+ AssignedToTable INT DEFAULT NULL,
  PRIMARY KEY (ReservationID),
  FOREIGN KEY (RestaurantAt) REFERENCES Restaurant (RestaurantID)
  );
 
  CREATE TABLE RestaurantTable(
- TableNumber int,
- RestaurantID int NOT NULL,
- NumOfSeats int NOT NULL,
- TableType char(3) NOT NULL,
- TableSection int,
+ TableNumber INT,
+ RestaurantID INT NOT NULL,
+ NumOfSeats INT NOT NULL,
+ TableType CHAR(3) NOT NULL,
+ TableSection INT,
  PRIMARY KEY (TableNumber, RestaurantID),
  FOREIGN KEY (RestaurantID) REFERENCES Restaurant (RestaurantID)
  );
 
  CREATE TABLE WaitStaff(
- WaiterID int,
- WaiterFirstName varchar(15),
- WaiterLastName varchar(15),
- WaiterSection int,
- WorksAt int,
+ WaiterID INT,
+ WaiterFirstName VARCHAR(15),
+ WaiterLastName VARCHAR(15),
+ WaiterSection INT,
+ WorksAt INT,
  PRIMARY KEY (WaiterID),
  FOREIGN KEY (WorksAt) REFERENCES Restaurant (RestaurantID)
  );
 
- /* SystemUser (UserID int, UserFirstName varchar(15), UserLastName varchar(15), UserPhone varchar(20), UserEmail varchar(50), UserRole char(3)) */
- insert into SystemUser values
+ /* SystemUser (UserID INT, UserFirstName VARCHAR(15), UserLastName VARCHAR(15), UserPhone VARCHAR(20), UserEmail VARCHAR(50), UserRole CHAR(3)) */
+ INSERT INTO SystemUser VALUES
  (115102704, 'Ian', 'McDowell', '+1 (484)-983-1943', 'ian.mcdowell@gmail.gmail', 'EMP');
-  insert into SystemUser values
+  INSERT INTO SystemUser VALUES
  (101121232, 'Rodolfo', 'Eduardo', NULL, 'raymond.castro@student.northampton.edu', 'MGR');
-  insert into SystemUser values
+  INSERT INTO SystemUser VALUES
  (127498344, NULL, NULL, NULL, NULL, 'GST');
-  insert into SystemUser values
+  INSERT INTO SystemUser VALUES
  (364748384, 'Sapphire', NULL, '+1 (484)-123-4567', NULL, 'EMP');
-  insert into SystemUser values
+  INSERT INTO SystemUser VALUES
  (120938743, NULL, 'Graver', '+1 (484)-987-1234', 'bg@email.org', 'EMP');
-  insert into SystemUser values
+  INSERT INTO SystemUser VALUES
  (987123654, 'Kieran', 'Culkin', NULL, 'KieranCulkin@hollywood.edu', 'MGR');
-  insert into SystemUser values
+  INSERT INTO SystemUser VALUES
  (123948173, NULL, NULL, NULL, NULL, 'GST');
 
- /* Restaurant (RestaurantID int, RestaurantName varchar(50) NN, RestaurantAddress varchar(50) NN, SeatingCapacity int NN, FloorPlan varbinary(max), Menu varbinary(max)) */
-insert into Restaurant values
+ /* Restaurant (RestaurantID INT, RestaurantName VARCHAR(50) NN, RestaurantAddress VARCHAR(50) NN, SeatingCapacity INT NN, FloorPlan VARBINARY(max), Menu VARBINARY(max)) */
+INSERT INTO Restaurant VALUES
 (110293012, 'Team Green''s Greens Bethlehem', '3835 Green Pond Road, Bethlehem PA, 18020', 20, NULL, NULL);
-insert into Restaurant values
+INSERT INTO Restaurant VALUES
 (102030405, 'Team Green''s Greens Poconos', '2411 Rt 715, Tannersville, PA 18372', 16, NULL, NULL);
 
- /* Reservation (ReservationID int, ResDate date NN,  ResTime time NN, PartySize int NN, PartyName varchar(30) NN, ResPhone varchar(20), ResEmail varchar(30), OptIns bit NN, ResStatus char(10) NN, DateCreated smalldatetime NN, Comments text, RestaurantAt int, AssignedToTable int DEFAULT NULL) */
- insert into Reservation values
+ /* Reservation (ReservationID INT, ResDate DATE NN,  ResTime TIME NN, PartySize INT NN, PartyName VARCHAR(30) NN, ResPhone VARCHAR(20), ResEmail VARCHAR(30), OptIns BIT NN, ResStatus CHAR(10) NN, DateCreated SMALLDATETIME NN, Comments VARCHAR(max), RestaurantAt INT, AssignedToTable INT DEFAULT NULL) */
+ INSERT INTO Reservation VALUES
  (324293838, '2026-03-29', '13:30:00', 2, 'Martha M', '+1 (727)-382-9823', 'MarthaMango@gmail.com', 0, 'IN FUTURE', '2026-03-24 21:38:43', NULL, 110293012, NULL);
-  insert into Reservation values
+  INSERT INTO Reservation VALUES
  (192334532, '2026-04-05', '16:00:00', 8, 'Angel Verdugo', NULL, NULL, 0, 'IN FUTURE', '2026-02-24 11:20:03', NULL, 110293012, NULL);
-  insert into Reservation values
+  INSERT INTO Reservation VALUES
  (132034111, '2026-04-05', '15:15:00', 4, 'LAURIE', NULL, 'laurieshotmail@hotmail.com', 1, 'IN FUTURE', '2026-03-20 20:08:14', 'Booth please', 102030405, NULL);
-  insert into Reservation values
+  INSERT INTO Reservation VALUES
  (129486949, '2026-04-04', '19:45:00', 2, 'Harold Corvin', '+1 (610)-332-2332', 'hcorvthemorv@gmail.com', 1, 'IN FUTURE', '2026-03-04 14:13:42', 'Need an allergen menu - Allergic to seafood', 102030405, NULL);
-  insert into Reservation values
+  INSERT INTO Reservation VALUES
  (102938465, '2026-03-20', '19:30:00', 2, 'Ramirez', '+1 (610)-610-6100', NULL, 1, 'FINISHED', '2026-02-24 12:08:34', NULL, 110293012, NULL);
-  insert into Reservation values
+  INSERT INTO Reservation VALUES
  (374583749, '2026-03-25', '17:00:00', 3, 'Bryson Stott', NULL, NULL, 0, 'WAITING', '2026-03-24 21:48:23', '32nd Birthday and 3rd Anniversary', 102030405, NULL);
-  insert into Reservation values
+  INSERT INTO Reservation VALUES
  (102938457, '2026-03-25', '13:30:00', 5, 'Martha M', NULL, NULL, 0, 'SEATED', '2026-03-01 08:59:01', NULL, 102030405, NULL);
 
-/* RestaurantTable (TableNumber int, RestaurantID int NN, NumOfSeats int NN, TableType char(3) NN, TableSection int) */
-insert into RestaurantTable values
+/* RestaurantTable (TableNumber INT, RestaurantID INT NN, NumOfSeats INT NN, TableType CHAR(3) NN, TableSection INT) */
+INSERT INTO RestaurantTable VALUES
 (1, 110293012, 8, 'BTH', 1);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (2, 110293012, 2, 'BTH', 1);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (3, 110293012, 4, 'TBL', 2);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (4, 110293012, 4, 'TBL', 2);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (5, 110293012, 2, 'BTH', 2);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (1, 102030405, 4, 'BTH', 1);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (2, 102030405, 4, 'TBL', 1);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (3, 102030405, 4, 'BTH', 2);
-insert into RestaurantTable values
+INSERT INTO RestaurantTable VALUES
 (4, 102030405, 4, 'TBL', 2);
 
-/* WaitStaff (WaiterID int, WaiterFirstName varchar(15), WaiterLastName varchar(15), WaiterSection int, WorksAt int) */
-insert into WaitStaff values
+/* WaitStaff (WaiterID INT, WaiterFirstName VARCHAR(15), WaiterLastName VARCHAR(15), WaiterSection INT, WorksAt INT) */
+INSERT INTO WaitStaff VALUES
 (102114783, 'Ian', 'McDowell', 2, 110293012);
-insert into WaitStaff values
+INSERT INTO WaitStaff VALUES
 (103493824, 'Yari', 'Perez', 1, 110293012);
-insert into WaitStaff values
+INSERT INTO WaitStaff VALUES
 (119283920, 'Brandon', 'Graver', 1, 102030405);
-insert into WaitStaff values
+INSERT INTO WaitStaff VALUES
 (183274928, 'Kuri', 'Edwards', 2, 102030405);
-insert into WaitStaff values
+INSERT INTO WaitStaff VALUES
 (123567890, 'Luke', 'Merlino', NULL, 102030405);
-insert into WaitStaff values
+INSERT INTO WaitStaff VALUES
 (147898654, 'Persephone', 'Harshbarger', NULL, 110293012);
 
 /*----------------QUERIES-------------------
@@ -148,15 +148,15 @@ Everything beginning with @ is an example/placeholder variable
 */
 
 -- Assign table to reservation
-DECLARE @UpdateReservationID int = 192334532
-DECLARE @UpdateReservationTable int = 1 
+DECLARE @UpdateReservationID INT = 192334532
+DECLARE @UpdateReservationTable INT = 1 
 UPDATE Reservation
 SET AssignedToTable = @UpdateReservationTable
 WHERE ReservationID = @UpdateReservationID
 
 -- Update status for reservation
-DECLARE @UpdateReservationStatusID int = 192334532
-DECLARE @UpdateReservationStatus char(10) = 'WAITING'
+DECLARE @UpdateReservationStatusID INT = 192334532
+DECLARE @UpdateReservationStatus CHAR(10) = 'WAITING'
 UPDATE Reservation
 SET ResStatus = @UpdateReservationStatus
 WHERE ReservationID = @UpdateReservationStatusID
@@ -174,15 +174,15 @@ FROM Reservation
 WHERE ResStatus != 'FINISHED'
 
 -- View reservation by ID
-DECLARE @SearchResID int = 324293838 
+DECLARE @SearchResID INT = 324293838 
 SELECT *
 FROM Reservation
 WHERE ReservationID = @SearchResID
 
 -- Find table details for reservation
-DECLARE @SearchReservationID int = 192334532
-DECLARE @SearchReservationTable int
-DECLARE @SearchReservationRestaurant int 
+DECLARE @SearchReservationID INT = 192334532
+DECLARE @SearchReservationTable INT
+DECLARE @SearchReservationRestaurant INT 
 SELECT @SearchReservationRestaurant = RestaurantAt, @SearchReservationTable = AssignedToTable
 FROM Reservation
 WHERE ReservationID = 192334532
